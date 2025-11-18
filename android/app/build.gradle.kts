a3ca90ba6f622a1ecfb5cd9d86ac1c4b062ad9e2
Dmitry Kouznetsov <dmitry.kouznetsov@protonmail.com>
2025-11-18 15:06:34 +0300

    Add abi version codes setter for building

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.api.ApkVariantOutputImpl
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -41,4 +43,17 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// F-droid requires variant.versionCode * 1000 + baseAbiVersionCode
+// instead of the flutter default: baseAbiVersionCode * 1000 + variant.versionCode
+val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
+android.applicationVariants.configureEach {
+    val variant = this
+    variant.outputs.forEach { output ->
+        val abiVersionCode = abiCodes[output.filters.find { it.filterType == "ABI" }?.identifier]
+        if (abiVersionCode != null) {
+            (output as ApkVariantOutputImpl).versionCodeOverride = variant.versionCode * 1000 + abiVersionCode
+        }
+    }
 }
